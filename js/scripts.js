88893bc8a877e5a06eafedc6d8de19fbd9ebdf01
Sploob <crayiphone@gmail.com>
2022-03-22 15:05:47 -0700

    add animation to elusive creature to run away on mouse hover
    Co-authored-by: William Rodriguez <william@rodriguez1.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
     $("img").removeClass("hidden");
   });
 
-  $("img").click(function() {
-    $("img").animate();
+  $("img").mouseenter(function() {
+    const height = $(window).height() * .9;
+    const width = $(window).width() * .9;
+
+    $("img").animate({
+      top: Math.floor(Math.random() * height),
+      left: Math.floor(Math.random() * width)
+    }, 250, function() {}
+    );
   });
 });
